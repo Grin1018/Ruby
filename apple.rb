1e8c2fd2d221fbf6fb6ca59580801b92c6f93137
EC2 Default User <ec2-user@ip-172-31-24-196.ap-northeast-1.compute.internal>
2022-10-06 01:25:07 +0000

    Ruby

---
+++ apple.rb
@@ -0,0 +1,11 @@
+apple = "Ymanashi"
+
+if apple == "Aomori"
+  puts "このリンゴ青森県産です。"
+  
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく長野県産です。"
+  
+else 
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
---
+++ greeting.rb
@@ -0,0 +1,7 @@
+def greeting(name)
+  return "Hello,#{name}!"
+  "Hello,#{name}!"
+  "Good mornig,#{name}!" 
+end
+
+puts greeting('John')
